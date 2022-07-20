4ec442120cb7c5b4ab76638fd4482b509534bf5e
bhagyarajuvarigeti1 <bhagyarajuvarigeti@gmail.com>
2022-07-20 09:39:59 +0530

    create a simple Linked lIst

---
+++ SimpleLinkedList.java
@@ -0,0 +1,12 @@
+import java.util.LinkedList;
+
+public class SimpleLinkedList {
+    public static void main(String[] args) {
+        LinkedList<Integer> lists = new LinkedList<>();
+        lists.add(56);
+        lists.add(30);
+        lists.add(70);
+
+        System.out.println(lists);
+    }
+}
